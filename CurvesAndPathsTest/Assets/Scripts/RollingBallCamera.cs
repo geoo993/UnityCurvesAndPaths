4c35a58c4e7009d8f61771773628016d7efbbe29
GEORGE QUENTIN <ggeoo93@gmail.com>
2016-06-07 19:17:39 +0100

    updated

---
+++ CurvesAndPathsTest/Assets/Scripts/RollingBallCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollingBallCamera : MonoBehaviour {
+
+	private float distance = 50.0f;
+	private float currentX = 0.0f;
+	private float currentY = 0.0f;
+
+	public Transform target;
+
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+		currentX += Input.GetAxis ("Vertical2");
+		currentY += Input.GetAxis ("Horizontal2");
+
+	}
+	void LateUpdate () {
+
+		FollowTargetWhenRolling ();
+
+	}
+	void FollowTargetWhenRolling ()
+	{
+		Vector3 dir = new Vector3 (0.0f, 0.0f, -distance);
+		Quaternion rotation = Quaternion.Euler (currentX, currentY, 0);
+		transform.position = target.position + rotation * dir;
+		transform.LookAt (target.position);
+
+	}
+}
---
+++ CurvesAndPathsTest/Assets/Scripts/RollingBallMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollingBallMovement : MonoBehaviour {
+
+	private Rigidbody rigid; 
+	public static float speed = 0.0f;// speed variable is the speed
+	private float moveVertical = 0.0f;
+	private float moveHorizontal = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown ("space")) 
+		{
+			
+			//print ("space");
+			this.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+			Vector3 look = new Vector3 (0.0f, Camera.main.gameObject.transform.position.y, Camera.main.gameObject.transform.position.z);
+			this.transform.LookAt(Camera.main.gameObject.transform.position,Camera.main.gameObject.transform.forward);
+
+		}
+	}
+	void FixedUpdate ()
+	{
+			BallMovement ();
+
+	}
+
+	void BallMovement(){
+		
+		speed = 200f;
+	
+		moveHorizontal = Input.GetAxis ("Horizontal"); 
+		moveVertical = Input.GetAxis ("Vertical");
+
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical) * (speed * 10) * Time.deltaTime;
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f) * (speed * 10) * Time.deltaTime;
+
+		//rigid.AddForce(Camera.main.gameObject.transform.forward * (moveVertical * 10) * Time.deltaTime);
+		rigid.AddForce((Camera.main.gameObject.transform.forward ) * moveVertical );
+
+
+
+	}
+}
